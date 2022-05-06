a255d1e6154e309ac12950e84bf1054e9181ab65
Irish Pearl <82577544+irish-pearl@users.noreply.github.com>
2022-05-06 22:08:34 +0800

    Add files via upload

---
+++ 6_getUserData_with_fgets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Using 'fgets' to grab a line of texts without breaking\n");
+    printf("\n");
+
+    char fullName[50];
+    printf("Enter your full name: ");
+    fgets(fullName, 50, stdin); //Variable name, maximum number of characters, "Standard Input"
+    printf("Welcome, %s", fullName);
+
+    /*Upon entering the data, there's an empty line under the last printf.
+    That's because when we hit enter, it gets stored as a new line character */
+
+    return 0;
+}
